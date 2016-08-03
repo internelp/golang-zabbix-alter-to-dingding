c96f12dd369164245282acced235af41d27d6729
internelp <gaof.d@foxmail.com>
2016-08-03 13:48:37 +0800

    增加了故障确认消息 修复了一个文本处理的bug

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type MsgInfo struct {
@@ -24,19 +25,21 @@ type MsgInfo struct {
 var msgInfo MsgInfo
 
 type Alter struct {
-	From         string `json:"from" xml:"from"`
-	Time         string `json:"time" xml:"time"`
-	Level        string `json:"level" xml:"level"`
-	Name         string `json:"name" xml:"name"`
-	Key          string `json:"key" xml:"key"`
-	Value        string `json:"value" xml:"value"`
-	Now          string `json:"now" xml:"now"`
-	ID           string `json:"id" xml:"id"`
-	IP           string `json:"ip" xml:"ip"`
-	Color        string `json:"color" xml:"color"`
-	Age          string `json:"age" xml:"age"`
-	Status       string `json:"status" xml:"status"`
-	RecoveryTime string `json:"recoveryTime" xml:"recoveryTime"`
+	From                   string `json:"from" xml:"from"`
+	Time                   string `json:"time" xml:"time"`
+	Level                  string `json:"level" xml:"level"`
+	Name                   string `json:"name" xml:"name"`
+	Key                    string `json:"key" xml:"key"`
+	Value                  string `json:"value" xml:"value"`
+	Now                    string `json:"now" xml:"now"`
+	ID                     string `json:"id" xml:"id"`
+	IP                     string `json:"ip" xml:"ip"`
+	Color                  string `json:"color" xml:"color"`
+	Age                    string `json:"age" xml:"age"`
+	Status                 string `json:"status" xml:"status"`
+	RecoveryTime           string `json:"recoveryTime" xml:"recoveryTime"`
+	Acknowledgement        string `json:"acknowledgement" xml:"acknowledgement"`
+	Acknowledgementhistory string `json:"acknowledgementhistory" xml:"acknowledgementhistory"`
 }
 type DingMsg struct {
 	Touser  string `json:"touser"`
@@ -72,49 +75,33 @@ func init() {
 	flag.StringVar(&msgInfo.Url, "url", "http://www.qiansw.com/golang-zabbix-alter-to-dingding.html", "消息内容点击后跳转到的URL，可空。")
 	flag.StringVar(&msgInfo.Style, "style", "json", "Msg的格式，可选json和xml，推荐使用xml（支持消息中含双引号），可空。")
 	flag.Parse()
-	log.Println("Init：初始化完成。")
-	//	msgInfo.Msg = fmt.Sprint(`
-	//			<?xml version="1.0" encoding="UTF-8" ?>
-	//			<xml>
-	//			<from>千思网</from>
-	//			<time>2016.07.28 17:00:05</time>
-	//			<level>Warning</level>
-	//			<name>这是一个千思网（qiansw.com）提供的ZABBIX"钉钉"报警插件。</name>
-	//			<key>icmpping</key>
-	//			<value>30ms</value>
-	//			<now>56ms</now>
-	//			<id>1637</id>
-	//			<ip>8.8.8.8</ip>
-	//			<color>FF4A934A</color>
-	//			<age>3m</age>
-	//			<recoveryTime>2016.07.28 17:03:05</recoveryTime>
-	//			<status>OK</status>
-	//			</xml>`)
+	log.Println("[Init] 初始化完成。")
 }
 
 func makeMsg(msg string) string {
 	//	根据json或xml文本创建消息体
-	log.Println("makeMsg：开始创建消息。")
+	log.Println("[makeMsg] 开始创建消息。")
 	var alter Alter
 	if msgInfo.Style == "xml" {
-		log.Println("makeMsg：来源消息格式为XML。")
+		log.Println("[makeMsg] 来源消息格式为XML。")
 		err := xml.Unmarshal([]byte(msg), &alter)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else if msgInfo.Style == "json" {
-		log.Println("makeMsg：来源消息格式为Json。")
+		log.Println("[makeMsg] 来源消息格式为Json。")
 		err := json.Unmarshal([]byte(msg), &alter)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		log.Println("makeMsg：未指定来源消息格式，默认使用Json解析。")
+		log.Println("[makeMsg] 未指定来源消息格式，默认使用Json解析。")
 		err := json.Unmarshal([]byte(msg), &alter)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+	log.Printf("[makeMsg] 来源消息为：%s。\r\n", msg)
 	var dingMsg DingMsg
 	//给dingMsg各元素赋值
 	dingMsg.Touser = msgInfo.To
@@ -137,6 +124,9 @@ func makeMsg(msg string) string {
 	if alter.Status == "PROBLEM" {
 		//  故障处理
 		dingMsg.Oa.Body.Author = fmt.Sprintf("[%s·%s(%s)]", alter.From, "故障", alter.ID)
+		if strings.Replace(alter.Acknowledgement, " ", "", -1) == "Yes" {
+			dingMsg.Oa.Body.Content = "故障已经被确认，" + alter.Acknowledgementhistory
+		}
 	} else if alter.Status == "OK" {
 		//  恢复处理
 		dingMsg.Oa.Body.Form[0].Key = "故障时间："
@@ -150,13 +140,16 @@ func makeMsg(msg string) string {
 		dingMsg.Oa.MessageURL = "http://www.qiansw.com/golang-zabbix-alter-to-dingding.html"
 		dingMsg.Oa.Body.Content = "ZABBIX动作配置有误，请至千思网[qiansw.com]或直接[点击此消息]查看具体配置文档。"
 		dingMsg.Oa.Body.Author = fmt.Sprintf("[%s·%s(%s)]", alter.From, alter.Status, alter.ID)
-
+		if strings.Replace(alter.Acknowledgement, " ", "", -1) == "Yes" {
+			dingMsg.Oa.Body.Content = "故障已经被确认，" + alter.Acknowledgementhistory
+		}
 	}
 	//	创建post给钉钉的Json文本
 	JsonMsg, err := json.Marshal(dingMsg)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("[makeMsg] 消息创建完成：%s\r\n", string(JsonMsg))
 	return string(JsonMsg)
 }
 
@@ -198,16 +191,13 @@ func getToken(corpid, corpsecret string) (token string) { //根据id和secret获
 		return
 	} else {
 		log.Fatal("Token解析失败！")
-
 		return
 	}
 
 }
 func sendMsg(token, msg string) (status bool) { //发送OA消息，,返回成功或失败
-
-	JsonMsg := fmt.Sprintf(msg)
-
-	body := bytes.NewBuffer([]byte(JsonMsg))
+	log.Printf("[sendMsg] 需要POST的内容：%s\r\n", msg)
+	body := bytes.NewBuffer([]byte(msg))
 	url := fmt.Sprintf("https://oapi.dingtalk.com/message/send?access_token=%s", token)
 	//	fmt.Println(url)
 	res, err := http.Post(url, "application/json;charset=utf-8", body)
